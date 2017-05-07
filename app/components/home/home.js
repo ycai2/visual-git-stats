5fd3889f85349e3347f16494e467c3dd8acd4d47
Yisheng Cai <cai.eason@gmail.com>
2017-05-07 19:44:22 -0400

    refactor: fix lint

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -1,6 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import styles from './styles/home.css';
 
 export default class Home extends Component {
